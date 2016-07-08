60b145e4d3322b4ec17e3d090aea5d723912076d
Mr. Behl <tbehl@student.neumont.edu>
2016-07-08 14:31:33 -0600

    Finished crawl and visit methods, beginning testing

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,8 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-public class Crawler {
+public class Crawler implements VisitAction {
 	
 	ArrayList<URL> toVisit = new ArrayList<URL>();
 	ArrayList<URL> beenVisit = new ArrayList<URL>();
@@ -20,6 +25,26 @@ public class Crawler {
 			toVisit.remove(currentURL);
 			beenVisit.add(currentURL);
 			visitMethodHolder.action(currentURL);
+		}
+	}
+
+	@Override
+	public void action(URL URLtoVisit) {
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(URLtoVisit.openStream()));
+			String strLine;
+			while((strLine = in.readLine()) != null){
+				String pattern = ".*[Aa]\\s+[Hh][Rr][Ee][Ff]\\s*=\\s*\"(.*?)\".*";
+				Pattern p = Pattern.compile(pattern);
+				Matcher m = p.matcher(strLine);
+				if(m.matches()){
+					toVisit.add(new URL(URLtoVisit.getProtocol() + "://" + URLtoVisit.getHost() 
+							+ strLine));
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("IO Exception.");
+			e.printStackTrace();
 		}
 	}
 }
